3f94d556f2c153408a210650c054870cb1baa25a
Arseniy Cherednichenko <ars7ars3@gmail.com>
2026-06-08 01:21:53 +0200

    Routes: register /about, /playground, /uses

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import Home from './pages/Home'
 import WorkDetail from './pages/WorkDetail'
+import About from './pages/About'
+import Playground from './pages/Playground'
+import Uses from './pages/Uses'
 import NotFound from './pages/NotFound'
 
 // Route table. The Layout holds the persistent chrome (background, nav, footer)
@@ -11,6 +14,9 @@ export default function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="playground" element={<Playground />} />
+        <Route path="uses" element={<Uses />} />
         <Route path="work/:slug" element={<WorkDetail />} />
         <Route path="*" element={<NotFound />} />
       </Route>
