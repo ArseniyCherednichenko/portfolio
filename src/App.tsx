a8089bd3d00bf16ba7ca4be07b5adf4fa38de6b1
Arseniy Cherednichenko <ars7ars3@gmail.com>
2026-08-05 00:14:08 +0000

    Route /specimen to the Type specimen page

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const Colophon = lazy(() => import('./pages/Colophon'))
 const Answers = lazy(() => import('./pages/Answers'))
 const Craft = lazy(() => import('./pages/Craft'))
 const Design = lazy(() => import('./pages/Design'))
+const Specimen = lazy(() => import('./pages/Specimen'))
 const Resume = lazy(() => import('./pages/Resume'))
 const TerminalPage = lazy(() => import('./pages/Terminal'))
 const Changelog = lazy(() => import('./pages/Changelog'))
@@ -47,6 +48,7 @@ export default function App() {
         <Route path="answers" element={<Answers />} />
         <Route path="craft" element={<Craft />} />
         <Route path="design" element={<Design />} />
+        <Route path="specimen" element={<Specimen />} />
         <Route path="resume" element={<Resume />} />
         <Route path="terminal" element={<TerminalPage />} />
         <Route path="changelog" element={<Changelog />} />
