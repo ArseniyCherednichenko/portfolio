eb2ba65cf92c9f18ed411063b58d39cdf117252a
Arseniy Cherednichenko <ars7ars3@gmail.com>
2026-08-05 00:16:06 +0000

    Bump PAGE_COUNT to 22 for the Type specimen page

--- src/data/stats.ts
+++ src/data/stats.ts
@@ -15,7 +15,7 @@
 export const COMPONENT_COUNT = 121
 
 /** Distinct routed pages the site serves. */
-export const PAGE_COUNT = 21
+export const PAGE_COUNT = 22
 
 export interface SiteStat {
   value: number
